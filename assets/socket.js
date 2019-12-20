a439f6ca2a538bb60cc9be6d8702071ac89ad536
Barna Balazs <balazsbarni@gmail.com>
2019-12-20 10:34:08 +0100

    I forgot to commit... So here you go.

--- assets/socket.js
+++ assets/socket.js
@@ -3,9 +3,16 @@
 const socket = io();
 const form = document.querySelector('form');
 const message = document.getElementById('m');
+const messages = document.getElementById('messages');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-	socket.emit('chat message', message.value);
+  socket.emit('chat message', message.value);
   form.reset();
+});
+
+socket.on('chat message', msg => {
+  let li = document.createElement('li');
+  li.innerText = msg;
+  messages.appendChild(li);
 });
--- backend.js
+++ backend.js
@@ -4,27 +4,29 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('a user connected');
-  socket.on('disconnect',() => {
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
 });
 
 
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
